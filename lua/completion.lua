fae7edb2b0e721bfbd51b23be85178a424065c2e
Jack DeVries <jdevries3133@gmail.com>
2022-06-23 21:42:35 -0400

    fix completion mappings

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,6 +1,7 @@
 local cmp = require('cmp')
 
 
+
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
@@ -11,8 +12,20 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
--- lua/neovim.lua
+++ lua/neovim.lua
@@ -1,7 +1,7 @@
 -- config that is unique to neovim.
 
 vim.g.colors_name = "PaperColor"
-vim.opt.background = "light"
+vim.opt.background = "dark"
 vim.api.nvim_set_option('laststatus', 3)
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
