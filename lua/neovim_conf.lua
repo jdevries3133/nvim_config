8a30cdeeb09eff2a571b8453349c66ec3371a2ff
Jack DeVries <jdevries3133@gmail.com>
2022-11-05 14:38:14 -0400

    new colorscheme and key mapping

--- lua/neovim_conf.lua
+++ lua/neovim_conf.lua
@@ -1,7 +1,8 @@
 -- native config that is unique to neovim.
-vim.g.colors_name = "catppuccin"
+vim.g.colors_name = "gruvbox-material"
 vim.opt.background = "dark"
 vim.api.nvim_set_option('laststatus', 3)
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
 vim.opt.cmdheight = 0
+vim.cmd('nnoremap <leader>l :LspRestart<CR>')
