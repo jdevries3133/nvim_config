86ee14d88f2357f98129dbf15ceba882c49dab90
Jack DeVries <jdevries3133@gmail.com>
2022-12-27 09:50:11 -0500

    revise neovim config lua module
    * add note about cmdheight
    * change laststatus set to use the same API as the rest of the module

--- lua/neovim_conf.lua
+++ lua/neovim_conf.lua
@@ -4,8 +4,18 @@
 
 vim.g.colors_name = "catppuccin"
 vim.opt.background = "dark"
-vim.api.nvim_set_option('laststatus', 3)
+vim.opt.laststatus = 3
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
+
+-- This is the default, but serves as a reminder to set cmdheight = 0 when it
+-- becomes stable. The cmdheight = 0 option was introduced in neovim 0.8, but
+-- it's considered experimental and definitely feels experimental. There are
+-- some wonky interactions with plugins where it will show some text and prompt
+-- you to press "enter" to make it go away, requiring an extra keystroke to
+-- facilitate the zero-height UI at the bottom without causing you to lose
+-- important information. This is especially annoying when working with
+-- dadbod-ui, for example. Until the ergonomics improve, or the plugins are
+-- updated to support this setting better, I need to keep cmdheight = 1.
 vim.opt.cmdheight = 1
 vim.cmd('nnoremap <leader>l :LspRestart<CR>')
