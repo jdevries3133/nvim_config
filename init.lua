e04733234c422220dd5dee856823732341cf8190
Jack DeVries <jdevries3133@gmail.com>
2022-12-30 08:45:48 -0500

    fix: plugin file must be sourced first

--- init.lua
+++ init.lua
@@ -1,16 +1,12 @@
 -- Note: all lua modules imported via `require()` are in the ./lua directory.
 -- This is part of neovim's default behavior.
 
------------------------------ native config -----------------------------------
+-- ./plugins.vim contains the plugin list for vim-plug, a package manager for
+-- vim plugins
+vim.cmd("source $XDG_CONFIG_HOME/nvim/plugins.vim")
 
 require("neovim_conf") ------------------- general purpose neovim-specific
 require("vim_config_adapter") ------------ sources stuff from vim config (~/.vim)
-
------------------------------ plugin config -----------------------------------
-
--- ./plugins.vim contains all `Plug` directives for vim-plug
-vim.cmd("source $XDG_CONFIG_HOME/nvim/plugins.vim")
-
 require("lsp_conf") ---------------------- languages server protocol
 require("dap_conf") ---------------------- debugger adapter protocol
 require("cmp_conf") ---------------------- neovim-cmp (completion suggestions)
