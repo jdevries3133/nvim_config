0647ec054742f536783de32a9981db45981293f5
Jack DeVries <jdevries3133@gmail.com>
2023-02-21 07:53:29 -0500

    fix: spelling

--- lua/vim_config_adapter.lua
+++ lua/vim_config_adapter.lua
@@ -1,9 +1,8 @@
--- Sourcing thingsn from my vim config. You don't need to install my vim config,
+-- Sourcing things from my vim config. You don't need to install my vim config,
 -- it'll be installed by vim-plug since it's included in ../plugins.vim.
 
 local vim_config_dir = vim.fn.getenv("XDG_CONFIG_HOME") .. "/nvim/plugged/vim_config/"
 
 vim.cmd("source " .. vim_config_dir .. "common.vim")
 vim.cmd("source " .. vim_config_dir .. "snippets.vim")
-print(vim_config_dir .. "after")
 vim.opt.runtimepath:append(vim_config_dir .. "after")
