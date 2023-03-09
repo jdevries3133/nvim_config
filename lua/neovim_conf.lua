623c131129906f17a03ba9d75b0bed68299029af
Jack DeVries <jdevries3133@gmail.com>
2023-03-09 09:36:59 -0500

    feat: change colorscheme to catppuccin-frappe

--- lua/neovim_conf.lua
+++ lua/neovim_conf.lua
@@ -2,7 +2,7 @@
 -- config is in this repository because most of it is in `common.vim`, which
 -- must be fetched separately since it's part of my vim config.
 
-vim.g.colors_name = "catppuccin"
+vim.g.colors_name = "catppuccin-frappe"
 vim.opt.background = "dark"
 vim.opt.laststatus = 3
 vim.opt.showmode = false
