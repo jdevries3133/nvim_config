9d68b22235485dc82d212ffdc8c826ea4ae8788d
Jack DeVries <jdevries3133@gmail.com>
2023-03-27 09:37:38 -0400

    doc(dap): cleanup module and documentation

--- lua/dap_conf.lua
+++ lua/dap_conf.lua
@@ -4,8 +4,8 @@
 -- in the "mappings" section.
 --
 -- There is no "lsp config" equivalent for DAP, so each language has a bit of
--- assembly required. See the docs for each support language; so far, I have
--- support for Python, JavaScript/TypeScript, and Rust
+-- assembly required. The beginning of the config sections for each language
+-- at the bottom have directions for getting them setup.
 
 local dap = require('dap')
 local dapui = require('dapui')
@@ -44,61 +44,6 @@ set('n', '<space>O', dapui.open, mapopts)
 set('n', '<space>v', float_scopes, mapopts)
 set('v', '<space>v', dapui.eval, mapopts)
 
-
---------------------------------- dap config ----------------------------------
-
----- python ----
--- For python, you'll need to install the debugpy module:
---
---    python3 -m pip install debugpy
---
--- You will also need to ensure that `python3 -m debugpy --version` from the
--- command line works in order for neovim to be able to discover the module.
-
-dap.adapters.python = {
-  type = 'executable';
-  command = 'python3';
-  args = { '-m', 'debugpy.adapter' };
-}
-
-dap.configurations.python = {
-  {
-    name = "run current file (generic)",
-    type = "python",
-    request = "launch",
-    program = "${file}",
-    pythonPath = "python3",
-  },
-  {
-    name = "unittest (generic)",
-    type = "python",
-    request = "launch",
-    module = "unittest",
-    pythonPath = "python3"
-  },
-  {
-    name = "pytest (generic)",
-    type = "python",
-    request = "launch",
-    module = "pytest",
-    pythonPath = "python3"
-  }
-}
-
-
----- node ----
-
-require("dap-vscode-js").setup({
-  -- Path to vscode-js-debug installation; as you can see, I'm typically
-  -- cloning the whole repo (https://github.com/microsoft/vscode-js-debug.git),
-  -- and then compiling it locally.
-  debugger_path = os.getenv("HOME") .. "/repos/vscode-js-debug",
-  -- which adapters to register in nvim-dap
-  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, 
-  log_file_path = os.getenv("HOME") .. "/.cache/nvim/dap_vscode_js.log"
-})
-
-
 -- We will always try to load launch.json files if they're present; I find
 -- this generally better than catchall configs (like I have above for python).
 -- Most projects have too much stuff going on for a default config to work
@@ -118,52 +63,7 @@ require('dap.ext.vscode').load_launchjs(
   }
 )
 
-
--- lldb config came from https://www.reddit.com/r/neovim/comments/q2hxkg/anyone_using_nvimdap_with_codelldb/
--- (top comment)
-
--- this guy: https://marketplace.visualstudio.com/items?itemName=vadimcn.vscode-lldb
--- needs to be installed and on your $PATH for this to work
---
--- Then, execute codelldb --port 13000 before and during any debugging sessions
-dap.adapters.codelldb = {
-    type = 'server',
-    host = '127.0.0.1',
-    port = 13000
-}
-
-dap.configurations.c = {
-    {
-        type = 'codelldb',
-        request = 'launch',
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd()..'/', 'file')
-        end,
-        --program = '${fileDirname}/${fileBasenameNoExtension}',
-        cwd = '${workspaceFolder}',
-        terminal = 'integrated'
-    }
-}
-
-dap.configurations.cpp = dap.configurations.c
-
--- I prefer to have the launch.json per-project, but this guy's catch-all
--- debug setup is actually pretty generally useful, so I'll keep it around
--- dap.configurations.rust = {
---     {
---         type = 'codelldb',
---         request = 'launch',
---         program = function()
---             return vim.fn.input('Path to executable: ', vim.fn.getcwd()..'/', 'file')
---         end,
---         cwd = '${workspaceFolder}',
---         terminal = 'integrated',
---         sourceLanguages = { 'rust' }
---     }
--- }
-
 --------------------------------- ui ------------------------------------------
-
 
 dapui.setup({
   icons = { expanded = "▾", collapsed = "▸" },
@@ -229,3 +129,107 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close({})
 end
+
+--------------------------------- python --------------------------------------
+
+-- For python, you'll need to install the debugpy module:
+--
+--    python3 -m pip install debugpy
+--
+-- You will also need to ensure that `python3 -m debugpy --version` from the
+-- command line works in order for neovim to be able to discover the module.
+
+dap.adapters.python = {
+  type = 'executable';
+  command = 'python3';
+  args = { '-m', 'debugpy.adapter' };
+}
+
+dap.configurations.python = {
+  {
+    name = "run current file (generic)",
+    type = "python",
+    request = "launch",
+    program = "${file}",
+    pythonPath = "python3",
+  },
+  {
+    name = "unittest (generic)",
+    type = "python",
+    request = "launch",
+    module = "unittest",
+    pythonPath = "python3"
+  },
+  {
+    name = "pytest (generic)",
+    type = "python",
+    request = "launch",
+    module = "pytest",
+    pythonPath = "python3"
+  }
+}
+
+
+--------------------------------- node ----------------------------------------
+-- Clone `vscode-js-debug` and build it locally. Then, provide the path to
+-- the build on your machine. I am using:
+--
+-- revision: 59abbf94a713ad8db1dd441b3287d0e7f97c3fca
+-- repository: https://github.com/microsoft/vscode-js-debug.git
+
+require("dap-vscode-js").setup({
+  debugger_path = os.getenv("HOME") .. "/repos/vscode-js-debug",
+  -- which adapters to register in nvim-dap
+  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, 
+  log_file_path = os.getenv("HOME") .. "/.cache/nvim/dap_vscode_js.log"
+})
+
+
+
+--------------------------------- rust (lldb) ---------------------------------
+
+-- lldb config came from https://www.reddit.com/r/neovim/comments/q2hxkg/anyone_using_nvimdap_with_codelldb/
+-- (top comment)
+
+-- this guy: https://marketplace.visualstudio.com/items?itemName=vadimcn.vscode-lldb
+-- needs to be installed and on your $PATH for this to work
+--
+-- Then, execute `codelldb --port 13000` before and during any debugging
+-- sessions. This just needs to be running in the background somewhere.
+dap.adapters.codelldb = {
+    type = 'server',
+    host = '127.0.0.1',
+    port = 13000
+}
+
+dap.configurations.c = {
+    {
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd()..'/', 'file')
+        end,
+        --program = '${fileDirname}/${fileBasenameNoExtension}',
+        cwd = '${workspaceFolder}',
+        terminal = 'integrated'
+    }
+}
+
+dap.configurations.cpp = dap.configurations.c
+
+-- I prefer to have the launch.json per-project, but this guy's catch-all
+-- debug setup is actually pretty generally useful, so I'll keep it around. I
+-- like how he prompts for the path to the executable..
+-- dap.configurations.rust = {
+--     {
+--         type = 'codelldb',
+--         request = 'launch',
+--         program = function()
+--             return vim.fn.input('Path to executable: ', vim.fn.getcwd()..'/', 'file')
+--         end,
+--         cwd = '${workspaceFolder}',
+--         terminal = 'integrated',
+--         sourceLanguages = { 'rust' }
+--     }
+-- }
+
