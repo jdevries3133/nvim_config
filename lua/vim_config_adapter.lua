3f6b65096dfee8728118a54d5b1423b7143ae78e
Jack DeVries <jdevries3133@gmail.com>
2023-07-25 19:47:23 -0400

    docs(vim-adapter): update docs

--- lua/vim_config_adapter.lua
+++ lua/vim_config_adapter.lua
@@ -1,5 +1,6 @@
 -- Sourcing things from my vim config. You don't need to install my vim config,
 -- it'll be installed by vim-plug since it's included in ../plugins.vim.
+-- There are some details about this quirk in ../FORKING.md
 
 local vim_config_dir = vim.fn.getenv("XDG_CONFIG_HOME") .. "/nvim/plugged/vim_config/"
 
