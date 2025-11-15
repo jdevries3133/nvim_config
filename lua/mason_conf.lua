32d88c84886c4ebe539ff6534ddf0dffb764dab6
Jack DeVries <jdevries3133@gmail.com>
2025-11-15 12:09:30 -0500

    chore: remove stale stuff from lsp config

--- lua/mason_conf.lua
+++ lua/mason_conf.lua
@@ -7,8 +7,6 @@
 -- obviously varies between different languages.
 
 require("mason").setup({})
-require("mason-lspconfig").setup()
-
 
 local shortcuts = require("shortcuts").my_custom_shortcuts
 local apply_shortcut = require("shortcuts").apply_shortcut
@@ -22,7 +20,7 @@ apply_shortcut('n', shortcuts.lsp_diagnostic_set_loclist, vim.diagnostic.setlocl
 apply_shortcut(
   'n',
   shortcuts.diagnostic_put_errors_into_quickfixlist,
-  function ()
+  function()
     vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR })
   end,
   { noremap = true, silent = true }
@@ -33,60 +31,26 @@ local on_attach = function(_, bufnr)
   -- Enable completion triggered by <c-x><c-o> in insert mode
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   apply_shortcut('n', shortcuts.lsp_goto_definition, vim.lsp.buf.definition, bufopts)
   apply_shortcut('n', shortcuts.lsp_goto_references, vim.lsp.buf.references, bufopts)
   apply_shortcut('n', shortcuts.lsp_hover, vim.lsp.buf.hover, bufopts)
   apply_shortcut('n', shortcuts.lsp_rename, vim.lsp.buf.rename, bufopts)
-  -- Note: the JavaScript ecosystem has no official code formatter so this
-  -- might not work right. If you try to use this, it will dispatch to the
-  -- formatting behavior of ts-server, which may not be the code formatter you
-  -- want or use.
   apply_shortcut('n', shortcuts.lsp_format, vim.lsp.buf.format, opts)
 end
-
-
--- this location handler populates the quickfixlist but does not open the
--- `:copen` drawer
-local function location_handler(_, result, ctx)
-  vim.g.lsp_last_word = vim.fn.expand('<cword>')
-  if result == nil or vim.tbl_isempty(result) then
-    print(ctx.method, 'No location found')
-    return nil
-  end
-  local util = require('vim.lsp.util')
-  if vim.tbl_islist(result) then
-    if #result == 1 then
-      util.jump_to_location(result[1], 'utf-8', true)
-    elseif #result > 1 then
-      vim.fn.setqflist(util.locations_to_items(result, 'utf-8'))
-    end
-  else
-    util.jump_to_location(result, 'utf-8', true)
-  end
-end
-
-
-vim.lsp.handlers['textDocument/declaration'] = location_handler
-vim.lsp.handlers['textDocument/definition'] = location_handler
-vim.lsp.handlers['textDocument/typeDefinition'] = location_handler
-vim.lsp.handlers['textDocument/implementation'] = location_handler
-vim.lsp.handlers['textDocument/references'] = location_handler
 
 require("mason-lspconfig").setup_handlers {
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
-  function (server_name) -- default handler (optional)
+  function(server_name)  -- default handler (optional)
     require("lspconfig")[server_name].setup {
       on_attach = on_attach
     }
   end,
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
-  ["rust_analyzer"] = function ()
+  ["rust_analyzer"] = function()
     require("lspconfig")["rust_analyzer"].setup {
       on_attach = on_attach,
       settings = {
@@ -107,14 +71,14 @@ require("mason-lspconfig").setup_handlers {
       }
     }
   end,
-  ["lua_ls"] = function ()
+  ["lua_ls"] = function()
     local runtime_path = vim.split(package.path, ';')
     require("lspconfig")["lua_ls"].setup {
       on_attach = on_attach,
       settings = {
         Lua = {
           runtime = {
-            -- Tell the language server which version of Lua you're using 
+            -- Tell the language server which version of Lua you're using
             -- (most likely LuaJIT in the case of Neovim)
             version = 'LuaJIT',
             -- Setup your lua path
